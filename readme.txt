2f878909f8b966526b98cff64e5864207dd0cbb7
rafi365 <74255365+rafi365@users.noreply.github.com>
2024-03-01 02:10:21 +0000

    init template

---
+++ readme.txt
@@ -0,0 +1,1 @@
+https://codepen.io/valerite-dev/pen/RRQKze
---
+++ script.js
@@ -0,0 +1,11 @@
+function addClass() {
+    document.body.classList.add("sent");
+}
+
+function removeClass() {
+    document.body.classList.remove("sent");
+}
+
+sendLetter.addEventListener("click", addClass);
+
+
