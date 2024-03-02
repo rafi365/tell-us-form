b3beceb66ddf8bf18611903586aa8dd63a4f86e0
rafi365 <74255365+rafi365@users.noreply.github.com>
2024-03-02 02:50:05 +0000

    functional input page

--- api/index.js
+++ api/index.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/api/submitform',upload.none(), (req, res) => {
+  console.log(req.body)
+  res.send('post it!')
 })
 
 app.listen(port, () => {
--- script.js
+++ script.js
@@ -1,11 +1,64 @@
+let issending = false;
+
 function addClass() {
     document.body.classList.add("sent");
+    document.getElementById('submit_button').innerHTML = "Loading...."
+    document.getElementById('submit_button').disabled = true
+}
+
+function sendingUI() {
+    document.getElementById('submit_button').innerHTML = "Loading...."
+    document.getElementById('submit_button').disabled = true
+}
+function removesendingUI() {
+    document.getElementById('submit_button').innerHTML = "Send"
+    document.getElementById('submit_button').disabled = true
 }
 
 function removeClass() {
+    document.getElementById("myForm").reset();
     document.body.classList.remove("sent");
 }
 
-sendLetter.addEventListener("click", addClass);
+async function submitform(event){
+    event.preventDefault()
+    if(issending){
+        return
+    }
+    issending = true;
+    sendingUI();
+    
+    const input_msg = document.getElementById("msg").value;
+    const input_name = document.getElementById("name").value;
+    const input_email = document.getElementById("email").value;
+    console.log(input_msg)
+    console.log(input_name)
+    console.log(input_email)
+    const formdata = new FormData();
+    formdata.append("input_msg", input_msg);
+    formdata.append("input_name", input_name);
+    formdata.append("input_email", input_email);
+    await sendform(formdata);
+    addClass();
+    issending = false;
+    removesendingUI();
+}
 
+async function sendform(formdata){
+    const myHeaders = new Headers();
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: formdata,
+        redirect: "follow"
+    };
+    try {
+        const response = await fetch("http://localhost:3000/api/submitform", requestOptions);
+        const result = await response.text();
+        console.log(result)
+    } catch (error) {
+        alert(error);
+        console.error(error);
+    };
+}
 
